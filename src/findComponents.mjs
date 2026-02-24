f6edb62d404e7aa3aa6bd6fec653013a0959bc38
Gareth Devlin <garethlouisdevlin@gmail.com>
2026-02-24 11:51:47 +0000

    Update regex for TS interfaces

--- src/findComponents.mjs
+++ src/findComponents.mjs
@@ -11,8 +11,8 @@ export function findComponents(inputPath, ignorePatterns, isPascalCase) {
   return searchFiles(
     filterFilesNot(findFiles(inputPath), ignorePatterns),
     isPascalCase
-      ? /(export default function|export const|export default|export declare const) ([A-Z][a-zA-Z]+)/g
-      : /(export default function|export const|export default|export declare const) ([a-zA-Z]+)/g,
+      ? /(export default function|export const|export default|export declare const|export interface) ([A-Z][a-zA-Z]+)/g
+      : /(export default function|export const|export default|export declare const|export interface) ([a-zA-Z]+)/g,
     2,
   );
 }
